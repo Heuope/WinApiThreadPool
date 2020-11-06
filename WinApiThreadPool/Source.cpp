39bf1b38ea4ddb5b837613939da3523774cc336f
Heuope <konstantin.sedniov@gmail.com>
2020-11-06 04:29:58 +0300

    test program

--- WinApiThreadPool/Source.cpp
+++ WinApiThreadPool/Source.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+#include "ThreadPool.h"
+
+void Test(void* args)
+{
+	Sleep(100);
+	std::cerr << "hello world" << std::endl;
+}
+
 int main()
 {
+	ThreadPool tp(5);
+
+	for (int i = 0; i < 10; i++)
+	{
+		tp.run(Test, nullptr);
+	}
+
+	tp.waitAll();
 	return 0;
 }
